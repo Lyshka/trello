41af54d8cc3362517b12e7128959ae1cd2d05473
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-01-29 22:40:42 +0300

    add board page

---
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -0,0 +1,66 @@
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
+import { notFound, redirect } from "next/navigation";
+import { BoardNavbar } from "./_components/board-navbar";
+
+interface IBoardIdLayout {
+  children: React.ReactNode;
+  params: { boardId: string };
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { boardId: string };
+}): Promise<Metadata> {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    return {
+      title: "Board",
+    };
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  return {
+    title: board?.title || "Board",
+  };
+}
+
+const BoardIdLayout = async ({ children, params }: IBoardIdLayout) => {
+  const { orgId } = auth();
+
+  if (!orgId) {
+    redirect("/select-org");
+  }
+
+  const board = await db.board.findUnique({
+    where: {
+      id: params.boardId,
+      orgId,
+    },
+  });
+
+  if (!board) {
+    notFound();
+  }
+
+  return (
+    <div
+      className="relative h-full bg-no-repeat bg-cover bg-center"
+      style={{ backgroundImage: `url(${board.imageFullUrl})` }}
+    >
+      <BoardNavbar data={board} />
+      <div className="absolute inset-0 bg-black/10" />
+      <main className="relative pt-28 h-full">{children}</main>
+    </div>
+  );
+};
+export default BoardIdLayout;
--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,10 +1,24 @@
+import { startCase } from "lodash";
+
 import { ILayout } from "@/types/layoutTypes";
 import { OrgControl } from "./_components/org-control";
+import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const { orgSlug } = auth();
+
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 
 const OrganizationIdLayout = ({ children }: ILayout) => {
-  return <>
-  <OrgControl />
-  {children}
-  </>;
+  return (
+    <>
+      <OrgControl />
+      {children}
+    </>
+  );
 };
 export default OrganizationIdLayout;
