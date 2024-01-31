fbb055fdfb573991e57a9b5b2b9d5543a18a88af
Lyshka <53864647+Lyshka@users.noreply.github.com>
2024-01-31 15:24:55 +0300

    add card modal

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,3 +1,5 @@
+import { ModalProvider } from "@/components/providers/modal-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 import { ILayout } from "@/types/layoutTypes";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
@@ -5,8 +7,11 @@ import { Toaster } from "sonner";
 const PlatformLayout = ({ children }: ILayout) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
